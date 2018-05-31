285ecb3afac37fc7a89a10652d55f8a85d3afcf1
Volodymyr Zubenko <voz@ciklum.com>
2018-05-31 20:05:03 +0300

    Add job

--- config.groovy
+++ config.groovy
@@ -9,6 +9,15 @@ branches.each {
             git("git://github.com/${project}.git", branchName)
         }
         steps {
+            msBuild {
+                msBuildInstallation('MSBuild 1.8')
+                buildFile('dir1/build.proj')
+                args('check')
+                args('another')
+                passBuildVariables()
+                continueOnBuildFailure()
+                unstableIfWarnings()
+            }
             maven("test -Dproject.name=${project}/${branchName}")
         }
     }
