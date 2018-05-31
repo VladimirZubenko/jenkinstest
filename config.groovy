0be2e0f08143e67349c2b969dbe979914447379f
Volodymyr Zubenko <voz@ciklum.com>
2018-05-31 20:06:43 +0300

    Add job

--- config.groovy
+++ config.groovy
@@ -22,3 +22,16 @@ branches.each {
         }
     }
 }
+job('example') {
+    steps {
+        msBuild {
+            msBuildInstallation('MSBuild 1.8')
+            buildFile('dir1/build.proj')
+            args('check')
+            args('another')
+            passBuildVariables()
+            continueOnBuildFailure()
+            unstableIfWarnings()
+        }
+    }
+}
